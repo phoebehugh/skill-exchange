a824319ad362f708ea21295296bc3166ed8cbd4b
Sara OC <saramoo@hotmail.com>
2015-05-11 16:10:34 +0100

    user can create shareable

---
+++ db/migrate/20150511150136_create_shareables.rb
@@ -0,0 +1,10 @@
+class CreateShareables < ActiveRecord::Migration
+  def change
+    create_table :shareables do |t|
+      t.belongs_to :user, index: true, foreign_key: true
+      t.integer :skill_id
+
+      t.timestamps null: false
+    end
+  end
+end
